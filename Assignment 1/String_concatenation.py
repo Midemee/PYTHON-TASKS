009eeaf0195b78a4eb1aa61ab1ba845ed37994ab
Midemee <ashiwajuaramide@gmail.com>
2026-04-11 18:53:47 +0100

    Update

--- Assignment 1/String_concatenation.py
+++ Assignment 1/String_concatenation.py
@@ -3,5 +3,6 @@ String concatenation means joining two or more strings together using the + oper
 
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+print("Hello, " + first_name + " " + last_name + "," + " " + "You are " + age + "years old")
 
-print("Hello, " + first_name + " " + last_name)
