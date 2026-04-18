d9c4199cc659e2f322e631dbd578b0aa32ea883e
Midemee <ashiwajuaramide@gmail.com>
2026-04-18 20:22:44 +0100

    Update

--- Assignment /CHAPTER THREE/miles_per_gallon.py
+++ Assignment /CHAPTER THREE/miles_per_gallon.py
@@ -8,21 +8,20 @@ while miles != -1:
     gallons = int(input("Enter the gallons: "))
     total_miles += miles
     total_gallons += gallons
-    miles_per_gallon = miles / gallons
     trip_count +=1
-    print('Miles per gallon =' + miles_per_gallon)
-    
-    print('Combined miles per gallon = {combined_milespergallon:.2f}')
 
-miles = int(input("Enter the miles driven or -1 to quit: "))
-combined_milespergallon = total_miles / total_gallons
+    miles_per_gallon = miles / gallons
+    print(f'Miles per gallon = {miles_per_gallon:.2f}')
+    combined_milespergallon = total_miles / total_gallons
+    print(f'Combined miles per gallon = {combined_milespergallon:.2f}')
+    miles = int(input("Enter the miles driven or -1 to quit: "))
     
 if trip_count == 0:
     print("No trip was recorded")
 else:
-   print("The total trips is: " + trip_count);
-   print("The total miles driven is: " + total_miles);
-   print("The total gallons used is: " + total_gallons);
-   print("The final combined mile per gallon is: " + total_miles / total_gallons);
+   print(f"The total trips is: {trip_count}");
+   print(f"The total miles driven is: {total_miles}");
+   print(f"The total gallons used is: {total_gallons}");
+   print(f"The final combined mile per gallon is: {total_miles / total_gallons:.2f}");
 
 
