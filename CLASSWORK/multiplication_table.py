e1cd349638c2bbb562754d1de4abe5420878990d
Midemee <ashiwajuaramide@gmail.com>
2026-04-20 13:01:09 +0100

    Update

--- CLASSWORK/multiplication_table.py
+++ CLASSWORK/multiplication_table.py
@@ -1,13 +1,12 @@
 '''PSEUDOCODE
 Declare variable count and intialise to 1
 Use the for loop to count 10 times
-Increase the count by 1
 Multiply userInput by the count 
 print out the result'''
 
 number = int(input("Enter a number: "))
 for count in range(1, 11):
-    #print("number x " + count + " = " + (number * count))
+    #print(number, "x" + count + " = " + (number * count))
     print(f'{number} x {count} = {number * count}')
 
 
---
+++ CLASSWORK/patterns.py
@@ -0,0 +1,6 @@
+#PSEUDOCODE
+
+for count in range(5, 0, -1):
+    for number in range(count, 0, -1):
+        print(number, end='')
+    print()
