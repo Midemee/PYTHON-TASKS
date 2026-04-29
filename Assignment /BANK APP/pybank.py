e320cd48fa1161a47efeea5fe0f757194a43584e
Midemee <ashiwajuaramide@gmail.com>
2026-04-29 09:46:08 +0100

    Update

--- Assignment /BANK APP/pybank.py
+++
@@ -1,9 +0,0 @@
-def validate_email(email):
-
-    if len(email) < 8:
-        return False
-    if "@" not in email:
-        return False
-    if email.startswith("@") or email.endswith("@"):
-        return False
-    return True
--- Assignment /BANK APP/test_pybank.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import pybank
-
-class TestValidateEmail(unittest.TestCase):
-
-    def test_valid_email_returns_true(self):
-        actual = pybank.validate_email("mide@example.com")
-        expected = True
-        self.assertEqual(actual, expected)
